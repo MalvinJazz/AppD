48b175c550982124fb728cff822259a51e1e0a15
Melvin Juárez <malvinjazz@live.com>
2016-07-06 14:37:01 -0600

    html's sin uso eliminados.

--- venv/src/denuncia/models.py
+++ venv/src/denuncia/models.py
@@ -1,10 +1,8 @@
 from __future__ import unicode_literals
 
-from django.db.models.signals import post_delete
+# from django.db.models.signals import post_delete
 from django.dispatch import receiver
 from django.db import models
-
-# Create your models here.
 
 class Denuncia(models.Model):
     id = models.AutoField(primary_key=True)
