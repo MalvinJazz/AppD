7ea5c902213c252fc197a6c620def24516e790ef
Melvin Juárez <malvinjazz@live.com>
2016-07-14 18:34:36 -0600

    Api de denuncia modificada

--- venv/src/denuncia/api.py
+++ venv/src/denuncia/api.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from django.core.mail import EmailMultiAlternatives
+
 from tastypie.resources import (
                         ModelResource,
                         ALL,
@@ -43,6 +47,44 @@ class DenunciaResource(ModelResource):
         attribute='direccion',
         full=True
     )
+
+    def obj_create(self, bundle, **kwargs):
+
+        print type(bundle.data.get('file'))
+
+        # text_content = 'Denuncia'
+        # html_content = '<!DOCTYPE html><html><body><h1>' + str(motivo) + '''</h1></br>
+        #                     <h3> Nombre: ''' + str(denuncia.nombre) + '''<br>
+        #                     DPI: ''' + str(denuncia.dpi) + '''<br>
+        #                     Telefono: ''' + str(denuncia.telefono) + '''</h3></br>
+        #                     <h4>Direccion: ''' + str(denuncia.direccion) + ''',
+        #                     ''' + str(municipio) + ', ' + str(departamento) +'''.
+        #                     <i>(Con referencia en: '''+str(denuncia.referencia)+''')</i> </h4>
+        #                     </br> <h5> Denuncio: </h5></br> <p>
+        #                     ''' + str(denuncia.denuncia) + '''</p></body>
+        #                     <footer><i>Los archivos quedan a cargo de la
+        #                      entidad indicada.</i><br>
+        #                     <i>Todos los datos de este correo son
+        #                      confidenciales y no deben ser difundidos
+        #                     a nadie más que las entidades interesadas
+        #                      en ellos.</i></footer></html>'''
+        #
+        # from_email = '"Denuncia Movil" <denunciamovil@gmail.com>'
+        # to = vIn
+        # msg = EmailMultiAlternatives(motivo, text_content, from_email, to)
+        # msg.attach_alternative(html_content, "text/html")
+        # if request.FILES:
+        #     msg.attach(archivo.name,archivo.read(),archivo.content_type)
+        # msg.send()
+
+        bundle.obj = self._meta.object_class()
+
+        for key, value in kwargs.items():
+            setattr(bundle.obj, key, value)
+
+        bundle = self.full_hydrate(bundle)
+
+        return self.save(bundle)
 
     class Meta:
         queryset = Denuncia.objects.all()
