b8081542374943737a8f68852b72e25df4e67097
Melvin Juárez <malvinjazz@live.com>
2016-07-15 11:45:12 -0600

    Envia correo desde el servidor, pero no la imagen.

--- venv/src/denuncia/api.py
+++ venv/src/denuncia/api.py
@@ -11,6 +11,7 @@ from tastypie.authorization import Authorization
 from tastypie import fields
 
 from .models import Denuncia, Motivo
+from institucion.models import Correo
 from institucion.api import InstitucionResource
 from localizaciones.api import (
     DepartamentoResource,
@@ -50,32 +51,7 @@ class DenunciaResource(ModelResource):
 
     def obj_create(self, bundle, **kwargs):
 
-        print type(bundle.data.get('file'))
-
-        # text_content = 'Denuncia'
-        # html_content = '<!DOCTYPE html><html><body><h1>' + str(motivo) + '''</h1></br>
-        #                     <h3> Nombre: ''' + str(denuncia.nombre) + '''<br>
-        #                     DPI: ''' + str(denuncia.dpi) + '''<br>
-        #                     Telefono: ''' + str(denuncia.telefono) + '''</h3></br>
-        #                     <h4>Direccion: ''' + str(denuncia.direccion) + ''',
-        #                     ''' + str(municipio) + ', ' + str(departamento) +'''.
-        #                     <i>(Con referencia en: '''+str(denuncia.referencia)+''')</i> </h4>
-        #                     </br> <h5> Denuncio: </h5></br> <p>
-        #                     ''' + str(denuncia.denuncia) + '''</p></body>
-        #                     <footer><i>Los archivos quedan a cargo de la
-        #                      entidad indicada.</i><br>
-        #                     <i>Todos los datos de este correo son
-        #                      confidenciales y no deben ser difundidos
-        #                     a nadie más que las entidades interesadas
-        #                      en ellos.</i></footer></html>'''
-        #
-        # from_email = '"Denuncia Movil" <denunciamovil@gmail.com>'
-        # to = vIn
-        # msg = EmailMultiAlternatives(motivo, text_content, from_email, to)
-        # msg.attach_alternative(html_content, "text/html")
-        # if request.FILES:
-        #     msg.attach(archivo.name,archivo.read(),archivo.content_type)
-        # msg.send()
+        imgData = bundle.data.get('file')
 
         bundle.obj = self._meta.object_class()
 
@@ -84,7 +60,56 @@ class DenunciaResource(ModelResource):
 
         bundle = self.full_hydrate(bundle)
 
-        return self.save(bundle)
+        objeto = self.save(bundle)
+
+        denuncia = bundle.obj
+        municipio = denuncia.direccion.municipio
+        departamento = municipio.departamento
+
+        motivo = denuncia.motivo
+        vIn = motivo.institucion
+        vIn = Correo.objects.filter(institucion=vIn)
+
+        text_content = 'Denuncia'
+        html_content = '<!DOCTYPE html><html><body><h1>' + str(motivo) + '''</h1></br>
+                            <h3> Nombre: ''' + str(denuncia.nombre) + '''<br>
+                            DPI: ''' + str(denuncia.dpi) + '''<br>
+                            Telefono: ''' + str(denuncia.telefono) + '''</h3></br>
+                            <h4>Direccion: ''' + str(denuncia.direccion) + ''',
+                            ''' + str(municipio) + ', ' + str(departamento) +'''.
+                            <i>(Con referencia en: '''+str(denuncia.referencia)+''')</i> </h4>
+                            </br> <h5> Denuncio: </h5></br> <p>
+                            ''' + str(denuncia.denuncia) + '''</p></body>
+                            <footer><i>Los archivos quedan a cargo de la
+                             entidad indicada.</i><br>
+                            <i>Todos los datos de este correo son
+                             confidenciales y no deben ser difundidos
+                            a nadie más que las entidades interesadas
+                             en ellos.</i></footer></html>'''
+
+        from_email = '"Denuncia Movil" <denunciamovil@gmail.com>'
+        to = vIn
+        msg = EmailMultiAlternatives(motivo, text_content, from_email, to)
+        msg.attach_alternative(html_content, "text/html")
+
+        # if imgData:
+        #
+        #     missing_padding = 4 - len(imgData) % 4
+        #     if missing_padding:
+        #         imgData += b'='* missing_padding
+        #
+        #     fh = open("imageToSave.jpeg", "wb")
+        #     fh.write(imgData.decode('base64'))
+        #     fh.close()
+        #
+        #     print fh
+        #
+        #     msg.attach(imgData)
+
+
+        msg.send()
+
+        return objeto
 
     class Meta:
         queryset = Denuncia.objects.all()
