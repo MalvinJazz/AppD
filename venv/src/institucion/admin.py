fdb7a3ff291e3b7e1e55b7f157a7ef91cba821ae
Melvin Juárez <malvinjazz@live.com>
2016-09-30 17:31:59 -0600

    Modificacion en admin de Correo

--- venv/src/institucion/admin.py
+++ venv/src/institucion/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 
 from .models import Institucion,Correo
 
+class CorreoAdmin(admin.ModelAdmin):
+    list_display = [
+            'correo',
+            'institucion',
+            'municipio',
+            'get_departamento'
+            ]
+
 admin.site.register(Institucion)
 # admin.site.register(Sede)
 # admin.site.register(Telefono)
-admin.site.register(Correo)
+admin.site.register(Correo, CorreoAdmin)
