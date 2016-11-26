aafe43b305e08f4e157f0b35e725f480e604a4fc
Melvin Juárez <malvinjazz@live.com>
2016-11-26 12:52:17 -0600

    Documentación de denuncia/models.py terminada.

--- venv/src/denuncia/models.py
+++ venv/src/denuncia/models.py
@@ -29,6 +29,32 @@ Tabla de la BD para guardar las denuncias.
 --------------------------------------------------------------------------------
 |     longitud     |    FloatField   |  Localizacion exacta de la denuncia.    |
 --------------------------------------------------------------------------------
+|     denuncia     |    TextField    |  Descripcion de la denuncia, puede      |
+|                  |                 |  quedar en blanco.                      |
+--------------------------------------------------------------------------------
+|    referencia    |    CharField    |  Contiene un punto de referencia a la   |
+|                  |                 |  ubucación de la denuncia.              |
+--------------------------------------------------------------------------------
+|      fecha       |  DateTimeField  |  Fecha y hora de la denuncia.           |
+--------------------------------------------------------------------------------
+|      tipo        |    CharField    |  Tipo de la denuncia, solo se puede     |
+|                  |                 |  llenar con algún indice de             |
+|                  |                 |  'TIPO_CHOICES'.                        |
+--------------------------------------------------------------------------------
+|      motivo      |       FK        |  Instancia y relación con el modelo     |
+|                  |                 |  'Motivo'.                              |
+--------------------------------------------------------------------------------
+|     direccion    |       FK        |  Instancia y Relacion con el modelo     |
+|                  |                 |  'Direccion', de la app localizaciones. |
+--------------------------------------------------------------------------------
+|    __unicode__   |     funcion     |  Representación del obejto como una     |
+|                  |                 |  cadena, en este caso, es la 'denuncia'.|
+--------------------------------------------------------------------------------
+|     getSprite    |     funcion     |  Devuelve un entero con el valor de     |
+|                  |                 |  la funcion 'sprite' del modelo 'motivo'|
+|                  |                 |  para cortar la imagen de los marcadores|
+|                  |                 |  que se muestran en el mapa.            |
+--------------------------------------------------------------------------------
 """
 class Denuncia(models.Model):
 
@@ -63,17 +89,71 @@ class Denuncia(models.Model):
     def getSprite(self):
         return self.motivo.sprite()
 
-
+#Cuando se borra una denuncia, al motivo se le resta uno en cantidad
 @receiver(post_delete, sender=Denuncia)
 def denuncia_delete(sender, instance, **kwargs):
     instance.motivo.cantidad -= 1
     instance.motivo.save()
 
+#Cuando se inserta una denuncia, al motivo se le suma uno en cantidad
 @receiver(post_save, sender=Denuncia)
 def denuncia_save(sender, instance, **kwargs):
     instance.motivo.cantidad += 1
     instance.motivo.save()
 
+"""
+Tabla de la BD que contiene los motivos que se pueden asociar a las denuncias.
+
+--------------------------------------------------------------------------------
+|     VARIABLE     |       TIPO      |              DESCRIPCION                |
+-------------------+-----------------+------------------------------------------
+|   TIPO_CHOICES   |      Tupla      |  Contiene a los tipos del motivo que se |
+|                  |                 |  pueden escoger. Se utilizan en la      |
+|                  |                 |  variable 'tipo'. Utiliza como indices  |
+|                  |                 |  a las 4 variables anteriores a ella.   |
+--------------------------------------------------------------------------------
+|        id        |       PK        |  Llave primaria de Motivo.              |
+|                  |      (INT)      |                                         |
+--------------------------------------------------------------------------------
+|      motivo      |    CharField    |  Motivo por el cual se puede denunciar  |
+--------------------------------------------------------------------------------
+|     cantidad     |   IntegerField  | Cantidad de denuncias por este motivo.  |
+--------------------------------------------------------------------------------
+|      tipo        |    CharField    |  Tipo del motivo, solo se puede llenar  |
+|                  |                 |  con algún indice de 'TIPO_CHOICES'.    |
+--------------------------------------------------------------------------------
+|   instituciones  | ManyToManyField |  Relacion muchos a muchos con el modelo |
+|                  |                 |  Institucion de la app institucion.     |
+--------------------------------------------------------------------------------
+|    __unicode__   |     funcion     |  Representación del obejto como una     |
+|                  |                 |  cadena, en este caso, es el 'motivo'.  |
+--------------------------------------------------------------------------------
+|    motivo_hash   |     funcion     |  Retorna el 'motivo' en formato hash md5|
+|                  |                 |  para poder usarlo como nombre de       |
+|                  |                 |  variable en JS al renderizar el        |
+|                  |                 |  template de estadisticas.              |
+--------------------------------------------------------------------------------
+|     sumTotal     |     funcion     |  Cantidad de denuncias por este motivo. |
+--------------------------------------------------------------------------------
+| get_instituciones|     funcion     |  Retorna una concatenanacion de los     |
+|                  |                 |  nombres de las instituciones           |
+|                  |                 |  relacionadas al motivo.                |
+--------------------------------------------------------------------------------
+|      sprite      |     funcion     |  Retorna un entero, resultado de una    |
+|                  |                 |  operación con el id, con el fin de     |
+|                  |                 |  encontrar la posición del marcador     |
+|                  |                 |  correspondiente al motivo en la imagen |
+|                  |                 |  '/static/images/marcadores.png'        |
+--------------------------------------------------------------------------------
+
+Meta:
+--------------------------------------------------------------------------------
+|   verbose_name   |      String     |  Plural del nombre del modelo.          |
+--------------------------------------------------------------------------------
+|     ordering     |      array      |  Lista que indica por que parametros    |
+|                  |                 |  se ordena la tabla del modelo.         |
+--------------------------------------------------------------------------------                                      
+"""
 class Motivo(models.Model):
     CRIMINAL = 'CR'
     MUNICIPAL = 'MU'
